6de398a3b051617f6c1867171a8cb40ca650389e
adenijimuiz <muizodunayo111@gmail.com>
2025-04-14 10:07:34 +0100

    dojo blog

---
+++ src/Home.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+import BlogList from './BlogList'
+import { Usefetch } from './Usefetch'
+
+
+export default function Home() {
+
+  const {data: blogs, isPending, error} = Usefetch('http://localhost:8000/blogs');
+
+  return (
+    <div className='home'>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading..</div>}
+      {blogs && <BlogList blogs = {blogs} title = 'All Blogs!'/>}
+
+
+
+      {/* <BlogList blogs = {blogs.filter((blog) => blog.author === 'mario')} title = 'Mario Blogs'/> to get blog with the author of mario */}
+      
+    </div>
+  )
+}
