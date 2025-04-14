095496a82a7dd54842ce6d0c9e7d9d5a0925934f
adenijimuiz <muizodunayo111@gmail.com>
2025-04-14 10:08:35 +0100

    custom hook for use hook

---
+++ src/usefetch.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+
+export const Usefetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+    useEffect(() => {
+      setTimeout(() => {
+        //don't use the setTimeout in real applications we are using it here because we are fetching in localhost
+        fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("could not fetch data");
+          }
+          // console.log(res)
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          // console.log(err.message)//instead of the console it is in the setError state
+          setError(err.message);
+          setIsPending(false);
+        })
+      },[3000]);
+    }, [url])
+
+    return{
+      data, isPending, error
+    }
+}
+
