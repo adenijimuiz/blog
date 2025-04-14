0bced0139d9280bc2d372a2db3dcfbcbad1217b5
adenijimuiz <muizodunayo111@gmail.com>
2025-04-14 14:55:44 +0100

    add the delete endpoint

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { Usefetch } from './Usefetch';
 
 export const BlogDetails = () => {
   const { id } = useParams();
-  const {data: blog, isPending, error} = Usefetch('http://localhost:8000/blogs/' + id)
+  const {data: blog, isPending, error} = Usefetch('http://localhost:8000/blogs/' + id);
+  const homeNav = useNavigate();
+
+  const handleDelete = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+      method: 'DELETE'
+    }).then(() => {
+      homeNav('/')
+    })
+  }
   return (
     <div className='blog-details'>
       {isPending && <div>Loading...</div>}
@@ -15,6 +24,7 @@ export const BlogDetails = () => {
           <p>Written by {blog.author}</p>
           <div>{blog.body}
           </div>
+          <button onClick={handleDelete}>delete</button>
         </article>
       )}
     </div>
